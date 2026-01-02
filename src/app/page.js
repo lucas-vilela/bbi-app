29750801ea55211bedacf5a917d2446ecfdb0141
Lucas Vilela <vilelalucas500@gmail.com>
2026-01-02 11:40:54 -0300

    single link

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,131 @@
-import Image from "next/image";
+"use client"
+import { Label } from "@/components/ui/label"
+import {
+  RadioGroup,
+  RadioGroupItem,
+} from "@/components/ui/radio-group"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator";
+import { ExternalLink, Hammer, PackageCheck } from "lucide-react";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
+  const [link, setLink] = useState('https://mav-qa.outback.com');
+  const [env, setEnv] = useState('mav-qa.');
+  const [brand, setBrand] = useState('outback');
+
+
+  useEffect(() => {
+    setLink(`https://${env}${brand}.com`)
+  }, [env, brand])
+
+
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="flex flex-col gap-4 min-h-screen w-full p-10">
+      <div className="w-full">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <Link target="_blank" href={link}>
+              <span className="flex items-center gap-1 font-medium">
+                {link}<ExternalLink size={12} />
+              </span>
+            </Link>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="w-full flex gap-4">
+        <div className="w-xs">
+          <Card className="w-full max-w-sm">
+            <CardHeader>
+              <CardTitle>Ambiente</CardTitle>
+              <CardDescription>Escolha qual será o ambiente.</CardDescription>
+              {/* <CardAction>Card Action</CardAction> */}
+            </CardHeader>
+            <CardContent>
+
+              <RadioGroup defaultValue="qa" >
+                <p className="flex gap-2 items-center"><span><Hammer size={14} /></span> Low</p>
+                <div className="flex items-center gap-3 rounded-md transition-all duration-300 hover:bg-slate-100 py-1 pl-1 pr-4 w-fit">
+                  <RadioGroupItem value="mav-dev." id="development" onClick={(e) => { setEnv(e.target.value) }} />
+                  <Label htmlFor="development">Development</Label>
+                </div>
+                <div className="flex items-center gap-3 rounded-md transition-all duration-300 hover:bg-slate-100 py-1 pl-1 pr-4 w-fit">
+                  <RadioGroupItem value="mav-qa." id="quality" onClick={(e) => { setEnv(e.target.value) }} />
+                  <Label htmlFor="quality">QA</Label>
+                </div>
+                <div className="flex items-center gap-3 rounded-md transition-all duration-300 hover:bg-slate-100 py-1 pl-1 pr-4 w-fit">
+                  <RadioGroupItem value="mav-stg." id="stage" onClick={(e) => { setEnv(e.target.value) }} />
+                  <Label htmlFor="stage">Stage</Label>
+                </div>
+                <Separator className="my-4" />
+
+                <p className="flex gap-2 items-center"><span><PackageCheck size={14} /></span> High</p>
+
+                <div className="flex items-center gap-3 rounded-md transition-all duration-300 hover:bg-slate-100 py-1 pl-1 pr-4 w-fit">
+                  <RadioGroupItem value="mav-preview." id="preview" onClick={(e) => { setEnv(e.target.value) }} />
+                  <Label htmlFor="preview">Preview</Label>
+                </div>
+                <div className="flex items-center gap-3 rounded-md transition-all duration-300 hover:bg-slate-100 py-1 pl-1 pr-4 w-fit">
+                  <RadioGroupItem value="" id="production" onClick={(e) => { setEnv(e.target.value) }} />
+                  <Label htmlFor="production">Production</Label>
+                </div>
+              </RadioGroup>
+            </CardContent>
+            {/* <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter> */}
+          </Card>
+
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="w-xs">
+          <Card className="w-full max-w-sm">
+            <CardHeader>
+              <CardTitle>Marca</CardTitle>
+              <CardDescription>Escolha qual será a marca do restaurante.</CardDescription>
+              {/* <CardAction>Card Action</CardAction> */}
+            </CardHeader>
+            <CardContent>
+
+              <RadioGroup defaultValue="obs">
+                <div className="flex items-center gap-3 rounded-md transition-all duration-300 hover:bg-slate-100 py-1 pl-1 pr-4 w-fit">
+                  <RadioGroupItem value="outback" id="outback" onClick={(e) => { setBrand(e.target.value) }} />
+                  <Label htmlFor="outback">Outback</Label>
+                </div>
+                <div className="flex items-center gap-3 rounded-md transition-all duration-300 hover:bg-slate-100 py-1 pl-1 pr-4 w-fit">
+                  <RadioGroupItem value="carrabbas" id="carrabbas" onClick={(e) => { setBrand(e.target.value) }} />
+                  <Label htmlFor="carrabbas">Carrabba's</Label>
+                </div>
+                <div className="flex items-center gap-3 rounded-md transition-all duration-300 hover:bg-slate-100 py-1 pl-1 pr-4 w-fit">
+                  <RadioGroupItem value="bonefishgrill" id="bonefish" onClick={(e) => { setBrand(e.target.value) }} />
+                  <Label htmlFor="bonefish">Bonefish Grill</Label>
+                </div>
+              </RadioGroup>
+            </CardContent>
+            {/* <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter> */}
+          </Card>
+
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
