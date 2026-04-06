046e327fc06003faf5f6f5f2a3d5491fbf5f73c8
Lucas Vilela <lucas.vilela@dtidigital.com.br>
2026-04-06 15:00:57 -0300

    Refactor layout and clean up unused code in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -243,7 +243,7 @@ export default function Home() {
                       <p className="text-sm leading-none font-medium w-7"></p>
                     </div> */}
                   </div>
-                  
+
                   <Separator className="my-6" />
 
                   <p className="flex gap-2 items-center"><Image src="/cig-favicon.png" width={20} height={10} alt="cig icon" /> Carrabba's</p>
@@ -255,6 +255,12 @@ export default function Home() {
                         </span>
                       </Link>
                     </div>
+                    {/* <div className="flex gap-2">
+                      <Separator orientation="vertical" />
+                      <p className="text-sm leading-none font-medium w-7"></p>
+                    </div> */}
+                  </div>
+                  <div className="flex justify-between rounded-md transition-all duration-300 hover:bg-slate-100 py-1 pl-1 pr-4 w-full">
                     <div className="flex items-center gap-3">
                       <Link target="_blank" href="carrabbas://specialReservation" className="flex w-fit">
                         <span className="flex items-center gap-2 w-fit text-sm">
@@ -262,9 +268,13 @@ export default function Home() {
                         </span>
                       </Link>
                     </div>
+                    {/* <div className="flex gap-2">
+                      <Separator orientation="vertical" />
+                      <p className="text-sm leading-none font-medium w-7"></p>
+                    </div> */}
                   </div>
-                  
-                  
+
+
                   <Separator className="my-6" />
 
                   <p className="flex gap-2 items-center"><Image src="/bfg-favicon.png" width={20} height={10} alt="bfg icon" /> Bonefish</p>
@@ -307,8 +317,8 @@ export default function Home() {
                       <p className="text-sm leading-none font-medium w-7"></p>
                     </div> */}
                   </div>
-                  
-                  
+
+
 
                 </CardContent>
                 {/* <CardFooter>
